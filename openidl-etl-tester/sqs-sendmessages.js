31e11194e17240963eb29629d3d79802f64fb4f6
kens-aais <kens@aaisonline.com>
2022-02-16 21:51:33 -0700

    make it possible to send many messages

--- openidl-etl-tester/sqs-sendmessages.js
+++ openidl-etl-tester/sqs-sendmessages.js
@@ -1,25 +1,25 @@
-var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({ region: 'us-east-1' });
-const config = require('./config/config.json')
+var AWS = require("aws-sdk");
+// Set the region
+AWS.config.update({ region: "us-east-1" });
+const config = require("./config/config.json");
 
-const messages = require('./test/samplemessages.json')
+const messages = require("./test/samplemessages.json");
 
-var sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
-
-for (message of messages) {
+var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+let argv = process.argv.slice(2);
+let loopCount = argv[0] ? parseInt(argv[0], 10) : 1;
+for (let step = 0; step < loopCount; step++) {
+  for (message of messages) {
     let params = {
-        MessageBody: JSON.stringify(message),
-        QueueUrl: config.stagingQueueUrl,
-        DelaySeconds: 5,
-        MessageAttributes: {
-
-        }
-
-    }
+      MessageBody: JSON.stringify(message),
+      QueueUrl: config.inputQueueUrl,
+      DelaySeconds: 5,
+      MessageAttributes: {},
+    };
 
     sqs.sendMessage(params, (err, data) => {
-        if (err) console.log(err, err.stack)
-        else console.log(data)
-    })
+      if (err) console.log(err, err.stack);
+      else console.log(data);
+    });
+  }
 }
