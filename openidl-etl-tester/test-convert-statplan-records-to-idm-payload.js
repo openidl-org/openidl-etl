a08b66311c6d82e961308752f3771977e4886748
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-08-03 14:48:41 -0400

    integrating loss records

--- openidl-etl-tester/test-convert-statplan-records-to-idm-payload.js
+++ openidl-etl-tester/test-convert-statplan-records-to-idm-payload.js
@@ -1,31 +1,59 @@
-const fs = require('fs')
+const fs = require("fs");
+const convertToJson =
+  require("../openidl-etl-statplan-processor/processor").convertTextRecordsToJson;
+const autoConverter =
+  require("../openidl-etl-statplan-processor/converters/autoConverter").converter;
+const buildPayload =
+  require("../openidl-etl-success-processor/load-insurance-data").buildPayload;
 
-const convertToJson = require('../openidl-etl-statplan-processor/processor').convertTextRecordsToJson
-const autoConverter = require('../openidl-etl-statplan-processor/converters/autoConverter').converter
-const buildPayload = require('../openidl-etl-success-processor/load-insurance-data').buildPayload
+//let testPremiumRecordsText = fs.readFileSync('../../con-data/auto_6350.txt', 'utf-8')
+let testPremiumRecordsText = fs.readFileSync("../../con-data/auto_6.txt","utf-8");
 
-//let testPremiumRecordsText = fs.readFileSync('./test/PA_Premium_Output_v1.1.txt', 'utf-8')
-let testPremiumRecordsText = fs.readFileSync('../../con-data/auto_6350.txt', 'utf-8')
-let jsonPremiumRecords = convertToJson(testPremiumRecordsText)
-//let jsonPremiumRecords = []
-console.log('idm converter| jsonpremium records length: '+jsonPremiumRecords.length)
-let hdsPremiumRecords = []
-for (jsonPremiumRecord of jsonPremiumRecords) {
-        //console.table(jsonPremiumRecord)
-        hdsPremiumRecords.push(autoConverter(jsonPremiumRecord))
-        //console.log('idm converter | record: '+jsonPremiumRecord)
+let jsonPremiumRecords = convertToJson(testPremiumRecordsText);
 
+// console.log(
+//   "idm converter| jsonpremium records length: " + jsonPremiumRecords.length
+// );
+let hdsPremiumRecords = [];
+let errorRecords = []
+for (let jsonPremiumRecord of jsonPremiumRecords) {
+  //console.table(jsonPremiumRecord)
+  console.log('id: '+jsonPremiumRecord.policyIdentification+' transactionCode: '+jsonPremiumRecord.transactionCode)
+  //transaction code 8,1 = premium
+
+  hdsPremiumRecords.push(autoConverter(jsonPremiumRecord));
+
+  // if (1==1){
+       
+
+  //     try {
+            
+  //             hdsPremiumRecords.push(autoConverter(jsonPremiumRecord));
+            
+  //         } catch (error) {
+  //           //console.table(jsonPremiumRecords)
+  //           console.log("UnLogged Error on: " + jsonPremiumRecord.policyIdentification);
+  //           errorRecords.push(jsonPremiumRecord)
+  //           console.log(error);
+  //         }
+  // }
 }
+console.log('hdsPremium length: '+hdsPremiumRecords.length)
+if (hdsPremiumRecords.length > 0) {
+  let premiumPayload = buildPayload(hdsPremiumRecords);
+  fs.writeFileSync("../../con-data/auto.json", JSON.stringify(premiumPayload));
+}
+fs.writeFileSync("../../con-data/error.json", JSON.stringify(errorRecords));
 
-let premiumPayload = buildPayload(hdsPremiumRecords)
+
+
+
+
+
+
+
 
 //fs.writeFileSync('./test/idmPremiumRecords.json', JSON.stringify(premiumPayload))
-
-fs.writeFileSync('../../con-data/auto.json', JSON.stringify(premiumPayload))
-
-
-
-
 // let testLossRecordsText = fs.readFileSync('./test/PA_Loss_Output_v1.1.txt', 'utf-8')
 // let jsonLossRecords = convertToJson(testLossRecordsText)
 // let hdsLossRecords = []
@@ -36,4 +64,3 @@ fs.writeFileSync('../../con-data/auto.json', JSON.stringify(premiumPayload))
 // let lossPayload = buildPayload(hdsLossRecords)
 
 // fs.writeFileSync('./test/idmLossRecords.json', JSON.stringify(lossPayload))
-
