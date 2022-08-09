78053dea754ddc82301d4ca2c10ed6830ab2fd04
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-08-09 16:10:52 -0400

    update order

--- openidl-etl-intake-processor/index.js
+++ openidl-etl-intake-processor/index.js
@@ -7,7 +7,7 @@
  * @author Ken Sayers
  */
 console.log('Loading function');
-
+const config = require('./config/config.json')
 let aws = require('aws-sdk');
 aws.config({region: config.region})
 
@@ -17,7 +17,7 @@ const ddb = new aws.DynamoDB({ apiVersion: '2012-08-10' })
 const convertToJson = require('./intake-to-json-processor').convertToJson
 const SecretsManager = require('./SecretsManager.js');
 
-const config = require('./config/config.json')
+
 
 // Handle the s3.putObject event
 exports.handler = async (event, context) => {
