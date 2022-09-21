4b1376ad9605e8ba35f379e3939161102d925ea7
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-09-21 14:12:43 -0400

    format doc

--- openidl-etl-intake-processor/intake-to-json-processor.js
+++ openidl-etl-intake-processor/intake-to-json-processor.js
@@ -1,93 +1,107 @@
-const csv = require('csvtojson')
-const DateTime = require('luxon').DateTime
-const config = require('./config/config.json')
-const crypto = require('crypto')
+const csv = require("csvtojson");
+const DateTime = require("luxon").DateTime;
+const config = require("./config/config.json");
+const crypto = require("crypto");
 async function convertToJson(recordsText) {
-    // let textRecords = recordsText.split('\n')
-    let errors = []
-    await csv()
-        .on('error', (err) => {
-            errors.push({ 'type': 'csv parsing', 'message': err })
-        })
-        .fromString(recordsText)
-        .then((jsonObj) => {
-            outputRecords = jsonObj
-        })
+  // let textRecords = recordsText.split('\n')
+  let errors = [];
+  await csv()
+    .on("error", (err) => {
+      errors.push({ type: "csv parsing", message: err });
+    })
+    .fromString(recordsText)
+    .then((jsonObj) => {
+      outputRecords = jsonObj;
+    });
 
-    if (errors.length > 0) {
-        return { 'valid': false, 'errors': errors }
+  if (errors.length > 0) {
+    return { valid: false, errors: errors };
+  }
+  if (outputRecords.length === 0) {
+    return { valid: false, errors: [{ type: "data", message: "empty data" }] };
+  }
+  let resultRecords = [];
+  for (outputRecord of outputRecords) {
+    console.log("output record");
+    console.table(outputRecord);
+    let resultRecord = {};
+    let recordError = false;
+
+    if (outputRecord["Organization ID"]) {
+      resultRecord.organizationID = outputRecord["Organization ID"];
     }
-    if (outputRecords.length === 0) {
-        return { 'valid': false, 'errors': [{ 'type': 'data', 'message': 'empty data' }] }
+
+    if (outputRecord["State"]) {
+      resultRecord.state = outputRecord["State"];
+    } else {
+      resultRecord.state = config.state;
     }
-    let resultRecords = []
-    for (outputRecord of outputRecords) {
-        console.log('output record')
-        console.table(outputRecord)
-        let resultRecord = {}
-        let recordError = false
-        
-        
-        if (outputRecord['Organization ID']){
-            resultRecord.organizationID = outputRecord['Organization ID']
-        }
 
-        if (outputRecord['State']){
-            resultRecord.state = outputRecord['State']
-        }else {
-            resultRecord.state = config.state
-        }
-        
-
-        if (outputRecord['VIN Hash']){
-            resultRecord.VINHash= outputRecord['VIN Hash']
-        }
-
-        if (!outputRecord['VINHash'])
-            if (!outputRecord.VIN){
-                errors.push({ 'type': 'data', 'message': 'missing "VIN"', 'record': outputRecord })
-            }
-            else {
-            
-            salt = outputRecord.VIN.substring(1,9)
-            resultRecord.VINHash = crypto.createHash('sha512',salt).update(outputRecord.VIN).digest('hex')
-            }
-
-        if (outputRecord['Transaction Date']){
-            resultRecord.transactionDate = outputRecord['Transaction Date']
-        }
-        
-        if (!resultRecord.organizationID) {
-            errors.push({ 'type': 'data', 'message': 'missing "Organization ID"', 'record': outputRecord })
-            console.log('error on org')
-            recordError = true
-        }
-        // console.log(outputRecord.VIN)
-        if (!outputRecord.VIN) {
-            if (!outputRecord['VIN Hash'])
-                {
-                    errors.push({ 'type': 'data', 'message': 'missing "VIN/Vin Hash"', 'record': outputRecord })
-                    console.log('error on vin')
-                    recordError = true
-                }
-            }
-        if (!resultRecord.transactionDate) {
-            errors.push({ 'type': 'data', 'message': 'missing "Transaction Date"', 'record': outputRecord })
-            console.log('error on org')
-            recordError = true
-        }
-
-        console.log('result: ')
-        console.table(resultRecord)
-       // console.log('errors')
-       // console.log(errors)
-        console.log('record error: '+recordError)
-        if (!recordError) resultRecords.push(resultRecord)
+    if (outputRecord["VIN Hash"]) {
+      resultRecord.VINHash = outputRecord["VIN Hash"];
     }
-    if (errors.length > 0) {
-        return { 'valid': false, 'errors': errors }
+
+    if (!outputRecord["VINHash"])
+      if (!outputRecord.VIN) {
+        errors.push({
+          type: "data",
+          message: 'missing "VIN"',
+          record: outputRecord,
+        });
+      } else {
+        salt = outputRecord.VIN.substring(1, 9);
+        resultRecord.VINHash = crypto
+          .createHash("sha512", salt)
+          .update(outputRecord.VIN)
+          .digest("hex");
+      }
+
+    if (outputRecord["Transaction Date"]) {
+      resultRecord.transactionDate = outputRecord["Transaction Date"];
     }
-    return { 'valid': true, 'records': resultRecords }
+
+    if (!resultRecord.organizationID) {
+      errors.push({
+        type: "data",
+        message: 'missing "Organization ID"',
+        record: outputRecord,
+      });
+      console.log("error on org");
+      recordError = true;
+    }
+    // console.log(outputRecord.VIN)
+    if (!outputRecord.VIN) {
+      if (!outputRecord["VIN Hash"]) {
+        errors.push({
+          type: "data",
+          message: 'missing "VIN/Vin Hash"',
+          record: outputRecord,
+        });
+        console.log("error on vin");
+        recordError = true;
+      }
+    }
+    if (!resultRecord.transactionDate) {
+      errors.push({
+        type: "data",
+        message: 'missing "Transaction Date"',
+        record: outputRecord,
+      });
+      console.log("error on org");
+      recordError = true;
+    }
+
+    console.log("result: ");
+    console.table(resultRecord);
+    // console.log('errors')
+    // console.log(errors)
+    console.log("record error: " + recordError);
+    if (!recordError) resultRecords.push(resultRecord);
+  }
+  if (errors.length > 0) {
+    return { valid: false, errors: errors };
+  }
+  return { valid: true, records: resultRecords };
 }
 
-module.exports.convertToJson = convertToJson
+module.exports.convertToJson = convertToJson;
