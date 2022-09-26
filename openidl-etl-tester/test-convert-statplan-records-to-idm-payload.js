b1127a3f4553076706f8374e6b751e8143d3080d
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-09-26 12:07:23 -0400

    update for sai

--- openidl-etl-tester/test-convert-statplan-records-to-idm-payload.js
+++ openidl-etl-tester/test-convert-statplan-records-to-idm-payload.js
@@ -6,9 +6,14 @@ const autoConverter =
 const buildPayload =
   require("../openidl-etl-success-processor/load-insurance-data").buildPayload;
 
+const config = require("./config/config.json")
 //let testPremiumRecordsText = fs.readFileSync('../../con-data/auto_6350.txt', 'utf-8')
-//let testPremiumRecordsText = fs.readFileSync('../../con-data/single-policy.txt', 'utf-8')
-let testPremiumRecordsText = fs.readFileSync("../../con-data/auto_1000.txt","utf-8");
+let testPremiumRecordsText = fs.readFileSync(config.inbound, 'utf-8')
+//let testPremiumRecordsText = fs.readFileSync("../../con-data/auto_1000.txt","utf-8");
+
+// if (testPremiumRecordsText.length > 10000){
+
+// }
 
 let jsonPremiumRecords = convertToJson(testPremiumRecordsText);
 // console.log(
