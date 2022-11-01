318f40b43bd6d54716375e35c9c1117bdd55fedf
Findlay Clarke <findlayc@AAISonline.com>
2022-11-01 13:56:28 -0400

    US101518: Upload UI | show control DB info

--- openidl-upload-ui/infra/src/control-table/read.js
+++ openidl-upload-ui/infra/src/control-table/read.js
@@ -2,6 +2,7 @@
 
 const AWSXRay = require("aws-xray-sdk-core");
 const AWS = AWSXRay.captureAWS(require("aws-sdk"));
+const responders = require("../helpers/responders");
 
 /**
  * @description Gets a items from dynamodb
@@ -9,18 +10,27 @@ const AWS = AWSXRay.captureAWS(require("aws-sdk"));
  * @author Findlay Clarke <findlayc@aaisonline.com>
  * @returns {* | null} the items from the control table
  */
-exports.handler = async orgId => {
+exports.handler = async (event, context, callback) => {
+  console.log(JSON.stringify(event));
+
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
   const params = {
-    TableName: process.env.ETL_CONTROL_TABLE
+    TableName: process.env.ETL_CONTROL_TABLE,
   };
 
   console.log("Sending params to dynamoDB", JSON.stringify(params));
 
-  const items = (await dynamoDb.scan(params).promise()).Items;
+  try {
+    const items = (await dynamoDb.scan(params).promise()).Items;
 
-  console.log("Got the items from the db", JSON.stringify(items));
-
-  return items;
+    console.log("Got the items from the db", JSON.stringify(items));
+    return callback(null, responders.get2XX(items));
+  } catch (e) {
+    console.log("failed to get data", e);
+    return callback(
+      null,
+      responders.getNon2XX(500, "An error occured. Please try again later")
+    );
+  }
 };
