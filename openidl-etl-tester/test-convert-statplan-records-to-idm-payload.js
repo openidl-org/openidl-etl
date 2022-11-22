95754d722a5a45ac480be2dad0a20243f9cf5344
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-11-22 13:13:05 -0500

    update sucess processor

--- openidl-etl-tester/test-convert-statplan-records-to-idm-payload.js
+++ openidl-etl-tester/test-convert-statplan-records-to-idm-payload.js
@@ -1,28 +1,38 @@
-const fs = require('fs')
+const fs = require("fs");
+const convertToJson =
+  require("../openidl-etl-statplan-processor/processor").convertTextRecordsToJson;
+const autoConverter =
+  require("../openidl-etl-statplan-processor/converters/autoConverter").converter;
+const buildPayload =
+  require("../openidl-etl-success-processor/load-insurance-data").buildPayload;
 
-const convertToJson = require('../openidl-etl-statplan-processor/processor').convertTextRecordsToJson
-const autoConverter = require('../openidl-etl-statplan-processor/converters/autoConverter').converter
-const buildPayload = require('../openidl-etl-success-processor/load-insurance-data').buildPayload
+const config = require("./config/config.json");
+let testPremiumRecordsText = fs.readFileSync(config.inbound, "utf-8");
 
-let testPremiumRecordsText = fs.readFileSync('./test/PA_Premium_Output_v1.1.txt', 'utf-8')
-let jsonPremiumRecords = convertToJson(testPremiumRecordsText)
-let hdsPremiumRecords = []
-for (jsonPremiumRecord of jsonPremiumRecords) {
-    hdsPremiumRecords.push(autoConverter(jsonPremiumRecord))
+let jsonPremiumRecords = convertToJson(testPremiumRecordsText);
+
+let hdsPremiumRecords = [];
+let errorRecords = [];
+for (let jsonPremiumRecord of jsonPremiumRecords) {
+  console.table(jsonPremiumRecord);
+
+  let id;
+  if ("policyIdentification" in jsonPremiumRecord) {
+    id = jsonPremiumRecord.policyIdentification;
+  }
+  if ("occurrenceIdentification" in jsonPremiumRecord) {
+    id = jsonPremiumRecord.occurrenceIdentification;
+  }
+
+  console.log(
+    "id: " + id + " transactionCode: " + jsonPremiumRecord.transactionCode
+  );
+
+  hdsPremiumRecords.push(autoConverter(jsonPremiumRecord));
 }
-
-let premiumPayload = buildPayload(hdsPremiumRecords)
-
-fs.writeFileSync('./test/idmPremiumRecords.json', JSON.stringify(premiumPayload))
-
-let testLossRecordsText = fs.readFileSync('./test/PA_Loss_Output_v1.1.txt', 'utf-8')
-let jsonLossRecords = convertToJson(testLossRecordsText)
-let hdsLossRecords = []
-for (jsonLossRecord of jsonLossRecords) {
-    hdsLossRecords.push(autoConverter(jsonLossRecord))
+console.log("hdsPremium length: " + hdsPremiumRecords.length);
+if (hdsPremiumRecords.length > 0) {
+  let premiumPayload = buildPayload(hdsPremiumRecords);
+  fs.writeFileSync(config.outbound, JSON.stringify(premiumPayload));
 }
-
-let lossPayload = buildPayload(hdsLossRecords)
-
-fs.writeFileSync('./test/idmLossRecords.json', JSON.stringify(lossPayload))
-
+fs.writeFileSync(config.error, JSON.stringify(errorRecords));
