8762acba73170eb999d9159198022007e28c0c55
Adnan C <adnan.choudhury@itpeoplecorp.com>
2023-02-21 12:58:44 -0500

    Test code
    Signed-off-by: Adnan C <adnan.choudhury@itpeoplecorp.com>

--- openidl-etl-intake-processor/test/intake-to-json-processor-test.js
+++ openidl-etl-intake-processor/test/intake-to-json-processor-test.js
@@ -2,9 +2,18 @@
 const fs = require('fs')
 const convertToJson = require('../intake-to-json-processor').convertToJson
 const test = async () => {
-	const inputRecords = fs.readFileSync('DateTest3Invalid.csv', 'utf-8')
-		result = await convertToJson(inputRecords)
-		console.log(result);
+	let inputRecords = fs.readFileSync('DateTest1.csv', 'utf-8')
+	result = await convertToJson(inputRecords)
+	console.log("First test: Should have dates in 'yyyy-MM-dd' format")
+	console.log(result); // Should have dates in "yyyy-MM-dd" format
+	inputRecords = fs.readFileSync('DateTest2.csv', 'utf-8')
+	result = await convertToJson(inputRecords)
+	console.log("Second test: Should have dates in''yyyy-MM-dd' format")
+	console.log(result); // Should have dates in "yyyy-MM-dd" format
+	inputRecords = fs.readFileSync('DateTest3Invalid.csv', 'utf-8')
+	result = await convertToJson(inputRecords)
+	console.log("Third test: Should have invalid date format error")
+	console.log(result); // should have errors
 }
 
 
